893c12f70032a5beb541084ca0e520340afda31d
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-21 18:11:44 +0100

    empty contact form on submit, display success or error message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,61 @@
+import { useState } from "react"
+
 function Contact() {
+  const [emailInput, setEmailInput] = useState("")
+  const [messageInput, setMessageInput] = useState("")
+  const [confirmationMessage, setConfirmationMessage] = useState(undefined)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (emailInput && messageInput) {
+      console.log(emailInput, messageInput)
+      setEmailInput("")
+      setMessageInput("")
+      setConfirmationMessage({ type: "success" })
+    } else {
+      setConfirmationMessage({ type: "error" })
+    }
+  }
+
   return (
     <div className="contact">
       <div className="form-group">
         <label htmlFor="exampleFormControlInput1">Din email</label>
         <input
           type="email"
+          value={emailInput}
           className="form-control"
           id="exampleFormControlInput1"
           placeholder="namn@exempel.se"
+          onChange={(event) => setEmailInput(event.target.value)}
         ></input>
       </div>
       <div className="form-group">
         <label htmlFor="exampleFormControlTextarea1">Ditt meddelande</label>
         <textarea
+          value={messageInput}
           className="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
+          onChange={(event) => setMessageInput(event.target.value)}
         ></textarea>
       </div>
+      {confirmationMessage?.type === "success" && (
+        <p id="thank-you-message">
+          Tack för ditt meddelande till oss på Lumia! Vi återkommer inom tre
+          arbetsdagar.
+        </p>
+      )}
+      {confirmationMessage?.type === "error" && (
+        <p id="error-message">Har du fyllt i dina uppgifter korrekt?</p>
+      )}
       <div className="d-flex justify-content-end">
-        <button type="button" className="btn btn-outline-dark">
+        <button
+          onClick={handleSubmit}
+          type="button"
+          className="btn btn-outline-dark"
+          style={{ marginTop: 10 + "px" }}
+        >
           Skicka ditt meddelande
         </button>
       </div>
