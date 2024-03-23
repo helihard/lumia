09c71a598ff4567a4b50a9e0c83d007b1693e210
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-23 19:40:08 +0100

    modal shows when product is added to cart

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,24 +1,39 @@
 import { CartContext } from "../context/CartContext.jsx"
 import { useContext, useState, useEffect } from "react"
+import Cart from "./Cart.jsx"
 
 const ProductsList = ({ filteredProducts }) => {
-  const { cartItems, addToCart } = useContext(CartContext)
+  const { addToCart } = useContext(CartContext)
+
+  const [showModal, setShowModal] = useState(false)
+
+  const handleModalClose = () => {
+    setShowModal(false)
+  }
+
+  const handleAddToCart = (product) => {
+    addToCart(product)
+    setShowModal(true)
+  }
 
   return (
-    <div className="lamps">
-      {filteredProducts.map((product) => (
-        <div className="product clearfix" key={product.id}>
-          <img src={product.image} alt={product.name}></img>
-          <div className="content">
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <button data-click="buy" onClick={() => addToCart(product)}>
-              Köp för {product.cost} kr
-            </button>
+    <>
+      <div className="lamps">
+        {filteredProducts.map((product) => (
+          <div className="product clearfix" key={product.id}>
+            <img src={product.image} alt={product.name}></img>
+            <div className="content">
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+              <button data-click="buy" onClick={() => handleAddToCart(product)}>
+                Köp för {product.cost} kr
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <Cart show={showModal} handleClose={handleModalClose} />
+    </>
   )
 }
 
