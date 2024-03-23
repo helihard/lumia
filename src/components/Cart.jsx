c2974e2a73483fe180a0c7c1d311582659d0bd0e
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-23 19:19:05 +0100

    functionality for displaying items and total in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,12 @@
 import { Modal, Button } from "react-bootstrap"
 import PropTypes from "prop-types"
+import { useContext } from "react"
+import { CartContext } from "../context/CartContext.jsx"
 
 const Cart = ({ show, handleClose }) => {
+  const { cartItems, addToCart, removeFromCart, emptyCart, getCartTotal } =
+    useContext(CartContext)
+
   return (
     <Modal
       show={show}
@@ -14,12 +19,23 @@ const Cart = ({ show, handleClose }) => {
       <div style={{ margin: 10 + "px" }}>
         <h3>Varukorg</h3>
         <ul>
-          <li></li>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <span>{item.cost}</span>
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="total">
-            Total <span>0</span>
-          </li>
+          {cartItems.length > 0 ? (
+            <li className="total">
+              Total <span>{getCartTotal()}</span>
+            </li>
+          ) : (
+            <li className="total">
+              Total <span>0</span>
+            </li>
+          )}
         </ul>
       </div>
       <Button variant="secondary" onClick={handleClose} id="modal-close-button">
