c2987bd4eba412329f5a715734d1bef59191b009
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-23 18:30:18 +0100

    added context to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import { CartProvider } from "./context/CartContext.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./styles/style.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 )
